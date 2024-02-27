523e611d25d0aecfeb5ff1aef4fe80aa843777a7
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2024-02-27 08:39:34 -0500

    installed sidebar plugins are now shown in plugin manager

--- react-devine/src/components/PluginsModal/index.tsx
+++ react-devine/src/components/PluginsModal/index.tsx
@@ -19,7 +19,7 @@ const PluginsModal: React.FC<Props> = ({ plugins }) => {
       className={cx(
         `absolute bg-black border-[0.5px] border-zinc-800 shadow-xl shadow-black/50 flex flex-col transition-[transform,opacity,width,height,border-radius] duration-300 z-50`,
         page === `installed`
-          ? `w-[600px] h-[400px] rounded-xl`
+          ? `w-[600px] h-[800px] max-h-screen rounded-xl`
           : `w-screen h-screen rounded-none`,
         state.mode === `plugins`
           ? `opacity-100`
@@ -57,8 +57,32 @@ const InstalledPluginsPage: React.FC<{ plugins: StudioConfig["plugins"] }> = ({
     </div>
   ) : (
     <div className="flex flex-col gap-4 p-4">
-      <div className="p-8 bg-zinc-900 rounded-lg"></div>
-      <div className="p-8 bg-zinc-900 rounded-lg"></div>
+      <div className="p-6 bg-zinc-900 rounded-lg">
+        <h2 className="text-white font-medium text-xl">Sidebar plugins</h2>
+        {plugins.sidebar && plugins.sidebar.length > 0 ? (
+          <div className="mt-4 grid grid-cols-2 gap-4">
+            {plugins.sidebar?.map((plugin) => (
+              <div className="p-4 bg-zinc-800 rounded-lg text-lg text-white font-medium flex justify-center items-center">
+                {plugin.title}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="text-zinc-400 text-lg flex justify-center my-4">
+            <span>No sidebar plugins</span>
+          </div>
+        )}
+      </div>
+      <div className="p-8 bg-zinc-900 rounded-lg">
+        <h2 className="text-white font-medium text-xl">Toolbar plugins</h2>
+        <div className="flex justify-center items-center bg-zinc-800 rounded-lg mt-4 p-4 text-lg text-zinc-300 gap-2 font-medium">
+          <span role="img" aria-label="sparkle">
+            ✨
+          </span>
+          {` `}
+          Coming soon!
+        </div>
+      </div>
     </div>
   );
 
